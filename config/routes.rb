d3f0d4e7c3b780f70b67ce60b7a5ebdb9a61f7d8
aikohi <aiko.t04.oar@gmail.com>
2019-07-24 12:05:42 +0000

    add: 会員登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    get 'top' => 'top#show'
 	 resources :books
+	 resources :users, except:[:edit,:update,:destroy]
+	 resources :sessions, only:[:new, :create, :destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_11_084335) do
+ActiveRecord::Schema.define(version: 2019_07_24_014914) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
